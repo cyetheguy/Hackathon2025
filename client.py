b6dc38316021ccc5cf9d9108bc395bd17e45e9a3
Sahunkuy <158181976+Sahunkuy@users.noreply.github.com>
2025-04-05 08:52:57 -0500

    Update client.py

--- client.py
+++ client.py
@@ -6,7 +6,7 @@ class Client:
     def __init__(self, HOST, PORT):
         self.socket = socket.socket()
         self.socket.connect((HOST, PORT))
-        ##self.name = input("Enter your name: ")  # Fixed typo "imput" to "input"
+        ##self.name = input("Enter your name: ") 
         ##self.talk_to_server()
 
     def talk_to_server(self):
@@ -17,11 +17,11 @@ class Client:
     def send_message(self):
         while True:
             client_input = input("")
-            client_message = self.name + ": " + client_input  # Added space after ":"
+            client_message = self.name + ": " + client_input  
             self.socket.send(client_message.encode())
 
     def send_m(self, m:str) -> None:
-        client_message = self.name + ": " + m  # Added space after ":"
+        client_message = self.name + ": " + m  
         self.socket.send(client_message.encode())
     
     def set_name(self, name):
@@ -33,9 +33,9 @@ class Client:
             server_message = self.socket.recv(1024).decode()
             if not server_message.strip():
                 os._exit(0)
-            print(server_message)  # Added to print the received message
+            print(server_message)  
 
 if __name__ == "__main__":
     client = Client('127.0.0.1', 7633)
-    client.name = input("Enter your name: ")  # Fixed typo "imput" to "input"
+    client.name = input("Enter your name: ")  
     client.talk_to_server()
